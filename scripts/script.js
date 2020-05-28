741bcddedee4cc1cfcf4d9b00cfba1379ead4c69
mkcvh <msmkcvh@gmail.com>
2020-05-28 19:44:20 +0200

    Finished JS core functionality

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,8 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
-addBookToLibrary("Harry Potter", "JK Rowling", 572, true);
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 240, true);
+addBookToLibrary("Harry Potter", "JK Rowling", 572, "Read");
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 240, "Not read");
 update();
 function render(obj) {
    let table = document.querySelector("table");
@@ -23,9 +23,24 @@ function render(obj) {
         dataCell.appendChild(dataText);
         row.appendChild(dataCell);
     }
+    let toggleCell = document.createElement("td");
+    let toggleBtn = document.createElement("BUTTON");
+    toggleBtn.innerHTML = obj.read == "Read"? "Not read" : "Read";
+    toggleBtn.setAttribute("class", "toggleButtons");
+    toggleBtn.addEventListener("click", function() {
+        if (obj.read == "Not read") {
+            obj.read = "Read";
+        } else {
+            obj.read = "Not read";
+        }
+        update();
+    })
+    toggleCell.appendChild(toggleBtn);
+    row.appendChild(toggleCell);
     let deleteCell = document.createElement("td");
     let deleteBtn = document.createElement("BUTTON");
     deleteBtn.innerHTML = "x";
+    deleteBtn.setAttribute("class", "deleteButtons");
     deleteBtn.addEventListener("click", function() {
         let index = myLibrary.indexOf(obj);
         myLibrary.splice(index, 1);
@@ -36,7 +51,7 @@ function render(obj) {
     table.appendChild(row);
 }
 
-document.getElementById("addBookButton").addEventListener("click", function(){ addBookToLibrary(document.getElementById('bookTitle').value, document.getElementById('bookAuthor').value,  parseInt(document.getElementById('bookPages').value), true );
+document.getElementById("addBookButton").addEventListener("click", function(){ addBookToLibrary(document.getElementById('bookTitle').value, document.getElementById('bookAuthor').value,  parseInt(document.getElementById('bookPages').value), document.getElementById('readTrue').checked? "Read" : "Not read" );
 document.querySelector(".addBook").style.visibility = "hidden";
 update();}
 );
